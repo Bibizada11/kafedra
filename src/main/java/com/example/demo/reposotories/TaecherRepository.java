93276b45417b3985a05fba190030a2970a43ce13
Биби <bibizadab1997@gmail.com>
2018-03-06 14:07:23 +0600

    агай жасадым, озыныз кордыныз гой

--- src/main/java/com/example/demo/reposotories/TaecherRepository.java
+++ src/main/java/com/example/demo/reposotories/TaecherRepository.java
@@ -4,7 +4,7 @@ import com.example.demo.models.Teacher;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.Optional ;
 @Repository
 public interface TaecherRepository extends CrudRepository<Teacher, Long> {
     Optional<Teacher> findById(Long id);
